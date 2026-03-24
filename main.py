9e43b5f2ab6066d9a85838d88e7a8651ed5ce2b6
abdulloherkinov667-create <abdulloherkinov667@gmail.com>
2026-03-24 23:03:45 +0500

    video yuklash qayta yozildi

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ from create import insert_user, users_table, create_user_pdf, get_all_users, che
 from buttons.inline import xabar_yubor
 from stets import SendImg
 
-API_TOKEN = "TOKENINGNI_QOY"
+API_TOKEN = "8301002449:AAFzKdU48I4Q0nuTxDnY9725MITFVA7w9ok"
 ADMIN_ID = [6411347321, 8327989068]
 
 bot = Bot(token=API_TOKEN)
